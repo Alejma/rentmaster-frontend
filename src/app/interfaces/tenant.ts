eec06b2db8be14e73d145c1e685c82ce2ef3e640
NdrsJmnz <julian.cerquera@correounivalle.edu.co>
2024-10-14 13:23:47 -0500

    Se agregó la lista de los arrendatarios al registrar un apartamento

--- src/app/interfaces/tenant.ts
+++ src/app/interfaces/tenant.ts
@@ -1,7 +1,8 @@
-export interface Tenant{
-    name: string,
-    email: string,
-    password: string,
-    payment_status: string,
-    phone_number: string,
+export interface Tenant {
+    tenant_id?: number; // Hacerlo opcional
+    name: string;
+    email: string;
+    password: string;
+    payment_status: string;
+    phone_number: string;
 }
