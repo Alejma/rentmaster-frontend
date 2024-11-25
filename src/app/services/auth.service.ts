ebd022464f567f61356393a61b4508982b85c90a
Cristian-ZG <zuniga.guti.cristian@gmail.com>
2024-11-25 15:24:12 -0500

    Se actualiza el servicio de autentificación para extraer el id del usuario logeado.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,40 +6,54 @@ import { Observable, tap } from 'rxjs';
 interface LoginResponse {
   token: string;
   role: string;
+  id: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
   private apiUrl = 'http://localhost:3001';
 
   constructor(private http: HttpClient, private router: Router) {}
 
+// Método para iniciar sesión
   login(email: string, password: string): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.apiUrl}/api/login`, { email, password }).pipe(
       tap(response => {
-        localStorage.setItem('token', response.token);
-        localStorage.setItem('role', response.role);
+        localStorage.setItem('token', response.token); // Guardamos el token
+        localStorage.setItem('role', response.role); // Guardamos el rol
+        localStorage.setItem('id', response.id); // Guardamos el ID del usuario
       })
     );
   }
 
-  getToken() {
+// Método para obtener el token almacenado
+  getToken(): string | null {
     return localStorage.getItem('token');
   }
 
-  getRole() {
+// Método para obtener el rol almacenado
+  getRole(): string | null {
     return localStorage.getItem('role');
   }
 
+// Método para obtener el ID del usuario almacenado
+  getId(): string | null {
+    return localStorage.getItem('id');
+  }
+
+// Método para verificar si el usuario está logeado
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
   }
 
+// Método para cerrar sesión
   logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem('token'); //Eliminamos el token, el rol y el id del local storage.
     localStorage.removeItem('role');
+    localStorage.removeItem('id');
     this.router.navigate(['/login']);
   }
 }
